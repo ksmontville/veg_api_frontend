14742299ee07bf3792eac0f21b168de2c25fa464
Kyle <ksmontville@gmail.com>
2022-09-29 13:24:11 -0400

    Configure TailwindCSS for production.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import NavbarComponent from './components/NavbarComponent.vue'
 import SearchComponent from './components/SearchComponent.vue'
 import ResultsComponent from './components/ResultsComponent.vue'
 import AddComponent from './components/AddComponent.vue'
-import axios from "axios";
+import './index.css'
 import './assets/main.css'
 
 const app = createApp(App)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      'index.html',
-      './src/App.vue',
-      './src/components/NavbarComponent.vue',
-      './src/components/SearchComponent.vue',
-      './src/components/ResultsComponent.vue',
-      './src/components/AddComponent.vue',
+      "./index.html",
+      "./src/components/AddComponent.vue",
+      "./src/components/ResultsComponent.vue",
+      "./src/components/SearchComponent.vue",
+      "./src/App.vue"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-      require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
