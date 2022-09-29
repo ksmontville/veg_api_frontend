492c6d6c529f60463c2e5227a5e2ed8c6495e4cf
Kyle <ksmontville@gmail.com>
2022-09-29 15:04:41 -0400

    Media query for background sizes.

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@
 main {
   background-image: url('/img/backgroundsmall.jpg');
   background-size: cover;
-  background-attachment: fixed;
+  background-attachment: scroll;
   background-position: center;
   background-repeat: no-repeat;
 }
@@ -39,7 +39,7 @@ main {
 @media (min-width: 768px) {
 
   main {
-      background-image: url('/img/background.jpg');
+    background-image: url('/img/background.jpg');
     background-attachment: fixed;
   }
 
