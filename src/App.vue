465cded563a85c1f171ca6d5dbdeb29b2058ac95
Kyle <ksmontville@gmail.com>
2022-09-29 14:40:36 -0400

    Fix parallax scrolling on mobile.

--- src/App.vue
+++ src/App.vue
@@ -31,14 +31,14 @@
 main {
   background-image: url('/img/background.jpg');
   background-size: cover;
-  background-attachment: scroll;
+  background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
 }
 
 @media(min-width: 768px) {
   main {
-     background-attachment: fixed;
+     background-attachment: scroll;
   }
 
 }
