7640288137f6712689347f97abf952dd71fef000
Kyle <ksmontville@gmail.com>
2022-10-22 11:23:26 -0400

    Add headers to POST requests.

--- src/components/AddComponent.vue
+++ src/components/AddComponent.vue
@@ -88,6 +88,10 @@ export default {
         'description': newVeggie.value.description,
         'procedure': newVeggie.value.procedure,
         'resource': newVeggie.value.resource
+      }, {
+        headers: {
+          'content-type': 'application/json'
+        }
       })
           clearAddForm()
     }
